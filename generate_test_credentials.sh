f7b559eeb283ece78309884d508d50964f273e60
ShailynOrtiz <sortiz@intellisys.com.do>
2019-09-27 03:45:59 -0400

    modifying the network to join containers in both docker-composes to the same network

--- generate_test_credentials.sh
+++ generate_test_credentials.sh
@@ -1,3 +1,6 @@
+echo "Generating a service token"
 export token=$(curl -H "X-Vault-Token: SuperSecretToken" \
-                      -X POST http://127.0.0.1:8200/v1/auth/token/create| jq '.auth.client_token')
+                      -X POST http://0.0.0.0:8200/v1/auth/token/create| jq '.auth.client_token')
 echo $token
+
+echo "Adding mongo-credentials"
